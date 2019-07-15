4af27c8f0f0f74cd760a3a65c0f0f934121e8e03
Arslan <arslanahmad5496@gmail.com>
2019-07-16 00:43:32 +0800

    Removed UserController code from ApplicationController for sepeation of concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,46 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   # Renders the home or index page
-  get '/signup' do
-    if !logged_in?
-      @user = User.new
-      erb :signup
-    else
-      redirect '/'
-    end
-  end
-  post '/signup' do
-    @user = User.new(username: params[:username], password: params[:password])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect '/'
-    else
-      erb :signup
-    end
-  end
-  get '/login' do
-    if !logged_in?
-      erb :login
-    else
-      redirect '/'
-    end
-  end
-  post '/login' do
-   user = User.find_by_username(params[:username])
-   if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect '/'
-   else
-    flash[:error] = "Invalid Credentials!"
-    redirect '/login'
-   end
-  end
-  get '/logout' do
-    if logged_in?
-      session.clear
-    end
-    redirect '/'
-  end
+  #Removed UserController Code. You can find it it in user_controller.rb under controllers directory. You May remove this comment after reading.
 
   # ===========ALI(posts)=============
   # creates a post
