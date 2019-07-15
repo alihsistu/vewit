86202da240f7f10ae8b449eb4c029ceacfb36cb5
Ali <alihsistu@gmail.com>
2019-07-15 16:06:52 +0800

    added likes column _ALI

--- db/migrate/20190715070913_create_posts.rb
+++ db/migrate/20190715070913_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.integer  :user_id
       t.string   :description
+      t.integer  :likes
     end
   end
 end
