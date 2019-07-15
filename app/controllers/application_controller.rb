b48d7a811a98a3ee2f57c5986d352778249c6e7c
Arslan <arslanahmad5496@gmail.com>
2019-07-15 13:35:26 +0800

    initialize sinatra base

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < Sinatra::Base
+
+  register Sinatra::ActiveRecordExtension
+
+  configure do
+  	set :views, "app/views"
+    set :public_dir, "public"
+    #enables sessions as per Sinatra's docs. Session_secret is meant to encript the session id so that users cannot create a fake session_id to hack into your site without logging in. 
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  # Renders the home or index page
+  get '/' do
+
+  end
+
+end
