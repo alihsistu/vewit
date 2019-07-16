d4492031490dee306ac8906dd5f369b261419afb
Om <hungomoct19@outlook.com>
2019-07-17 00:55:13 +0800

    user update approved

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,27 @@ class User < ActiveRecord::Base
     has_many :posts, foreign_key: :user_id, dependent: :destroy
     has_many :repliers, through: :posts
     has_many :followers, foreign_key: :follower_id, class_name: "Follower", dependent: :destroy
-<<<<<<< Updated upstream
     validates_presence_of :username
     validates_uniqueness_of :username
-=======
 
 
     # validates_presence_of :name,:username, :email
     # validates :email, uniqueness: true
 
->>>>>>> Stashed changes
     has_secure_password
+    #to use later
+
+    # include BCrypt
+  
+    # def password
+    #   @password ||= Password.new(password_hash)
+    # end
+  
+  
+    # def password=(new_password)
+    #   @password = Password.create(new_password)
+    #   self.password_hash = @password
+    # end
 
 
     def self.follow(username,current_user)
