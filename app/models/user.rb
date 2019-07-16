f320f8ca6456d4225722d19f4306b2266b772a95
Arslan <arslanahmad5496@gmail.com>
2019-07-16 19:03:45 +0800

    Added validation for username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,9 @@ class User < ActiveRecord::Base
     has_many :posts, foreign_key: :user_id, dependent: :destroy
     has_many :repliers, through: :posts
     has_many :followers, foreign_key: :follower_id, class_name: "Follower", dependent: :destroy
-
-
     validates_presence_of :username
-    # validates :email, uniqueness: true
-
+    validates_uniqueness_of :username
     has_secure_password
-    #to use later
-
-    # include BCrypt
-  
-    # def password
-    #   @password ||= Password.new(password_hash)
-    # end
-  
-  
-    # def password=(new_password)
-    #   @password = Password.create(new_password)
-    #   self.password_hash = @password
-    # end
 
 
     def follow!(user)
